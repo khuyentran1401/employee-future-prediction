12f5978e5cb5ee7e803d70dcfe36e76605036153
khuyentran1401 <khuyentran1476@gmail.com>
2022-04-18 11:10:33 -0500

    add code

--- src/process.py
+++ src/process.py
@@ -1,21 +1,52 @@
-"""
-This is the demo code that uses hydra to access the parameters in under the directory config.
-
-Author: Khuyen Tran
-"""
-
 import hydra
+import pandas as pd
 from hydra.utils import to_absolute_path as abspath
 from omegaconf import DictConfig
+from patsy import dmatrices
+
+
+def get_data(raw_path: str):
+    data = pd.read_csv(raw_path)
+    return data
+
+
+def get_features_without_intercept(
+    target: str, features: list, data: pd.DataFrame
+):
+    feature_str = " + ".join(features)
+    y, X = dmatrices(
+        f"{target} ~ {feature_str} - 1", data=data, return_type="dataframe"
+    )
+    return y, X
+
+
+def get_features_with_intercept(
+    target: str, features: list, data: pd.DataFrame
+):
+    feature_str = " + ".join(features)
+    y, X = dmatrices(
+        f"{target} ~ {feature_str}", data=data, return_type="dataframe"
+    )
+    return y, X
 
 
 @hydra.main(config_path="../config", config_name="main")
 def process_data(config: DictConfig):
     """Function to process the data"""
 
-    raw_path = abspath(config.raw.path)
-    print(f"Process data using {raw_path}")
-    print(f"Columns used: {config.process.use_columns}")
+    data = get_data(abspath(config.raw.path))
+
+    if config.process.has_intercept:
+        y, X = get_features_with_intercept(
+            config.process.target, config.process.features, data
+        )
+    else:
+        y, X = get_features_without_intercept(
+            config.process.target, config.process.features, data
+        )
+
+    y.to_csv(abspath(config.processed.target.path), index=False)
+    X.to_csv(abspath(config.processed.features.path), index=False)
 
 
 if __name__ == "__main__":
--- src/train_model.py
+++ src/train_model.py
@@ -1,25 +1,53 @@
-"""
-This is the demo code that uses hy                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      dra to access the parameters in under the directory config.
+import warnings
 
-Author: Khuyen Tran
-"""
+warnings.filterwarnings(action="ignore")
 
 import hydra
+import pandas as pd
 from hydra.utils import to_absolute_path as abspath
 from omegaconf import DictConfig
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+
+
+def load_data(target_path: str, feature_path: str):
+    y = pd.read_csv(target_path)
+    X = pd.read_csv(feature_path)
+    return y, X
+
+
+def rename_columns(X: pd.DataFrame, map_columns: dict):
+    return X.rename(columns=map_columns)
+
+
+def train_model(X_train: pd.DataFrame, y_train: pd.DataFrame):
+    return XGBClassifier(
+        use_label_encoder=False, objective="binary:logistic"
+    ).fit(X_train, y_train)
+
+
+def predict(X_test: pd.DataFrame, model: XGBClassifier):
+    return model.predict(X_test)
 
 
 @hydra.main(config_path="../config", config_name="main")
-def train_model(config: DictConfig):
+def main(config: DictConfig):
     """Function to train the model"""
 
-    input_path = abspath(config.processed.path)
-    output_path = abspath(config.final.path)
+    target_path = abspath(config.processed.target.path)
+    feature_path = abspath(config.processed.features.path)
 
-    print(f"Train modeling using {input_path}")
-    print(f"Model used: {config.model.name}")
-    print(f"Save the output to {output_path}")
+    y, X = load_data(target_path, feature_path)
+    X = rename_columns(X, config.map_columns)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=7
+    )
+    model = train_model(X_train, y_train)
+    prediction = predict(X_test, model)
+    f1 = f1_score(y_test, prediction)
+    print(f"F1 Score of this model is {f1}.")
 
 
 if __name__ == "__main__":
-    train_model()
+    main()
