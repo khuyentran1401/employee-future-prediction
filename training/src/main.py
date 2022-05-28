2499c9f6ce69fbc348675528d62ec308a18adabe
khuyentran1401 <khuyentran1476@gmail.com>
2022-05-28 15:52:42 -0500

    add run processing and training code

--- training/src/main.py
+++ training/src/main.py
@@ -3,7 +3,7 @@ from process import process_data
 from train_model import train
 
 
-@hydra.main(version_base=None, config_path="../config", config_name="main")
+@hydra.main(version_base=None, config_path="../../config", config_name="main")
 def main(config):
     process_data(config)
     train(config)
