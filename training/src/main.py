716bfbce5ada89a2d845cdbd6b2b68a8c7552ed9
khuyentran1401 <khuyentran1476@gmail.com>
2022-05-29 12:59:49 -0500

    use process 1

--- training/src/main.py
+++ training/src/main.py
@@ -1,12 +1,14 @@
 import hydra
 from process import process_data
 from train_model import train
+from evaluate_model import evaluate
 
 
 @hydra.main(version_base=None, config_path="../../config", config_name="main")
 def main(config):
     process_data(config)
     train(config)
+    evaluate(config)
 
 
 if __name__ == "__main__":
