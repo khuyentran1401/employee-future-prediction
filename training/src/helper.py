57b6e6cd1fe68934c470a43906803ae8e5f1ae6d
Khuyen Tran <khuyentran1476@gmail.com>
2022-08-11 10:55:19 -0500

    fix process.py

--- training/src/helper.py
+++ training/src/helper.py
@@ -1,15 +1,10 @@
-import mlflow
-from dagshub import DAGsHubLogger
+from hydra import compose, initialize
+from prefect import task
 
 
-class BaseLogger:
-    def __init__(self):
-        self.logger = DAGsHubLogger()
-
-    def log_metrics(self, metrics: dict):
-        mlflow.log_metrics(metrics)
-        self.logger.log_metrics(metrics)
-
-    def log_params(self, params: dict):
-        mlflow.log_params(params)
-        self.logger.log_hyperparams(params)
+@task
+def load_config():
+    """Load configurations from the file `main.yaml` under the `config` directory"""
+    with initialize(version_base=None, config_path="../../config"):
+        config = compose(config_name="main")
+    return config
--- training/src/process.py
+++ training/src/process.py
@@ -1,16 +1,17 @@
-import hydra
 import pandas as pd
-from hydra.utils import to_absolute_path as abspath
-from omegaconf import DictConfig
+from helper import load_config
 from patsy import dmatrices
+from prefect import flow, task
 from sklearn.model_selection import train_test_split
 
 
+@task
 def get_data(raw_path: str):
     data = pd.read_csv(raw_path)
     return data
 
 
+@task
 def get_features(target: str, features: list, data: pd.DataFrame):
     feature_str = " + ".join(features)
     y, X = dmatrices(
@@ -19,6 +20,7 @@ def get_features(target: str, features: list, data: pd.DataFrame):
     return y, X
 
 
+@task
 def rename_columns(X: pd.DataFrame):
     X.columns = X.columns.str.replace("[", "_", regex=True).str.replace(
         "]", "", regex=True
@@ -26,24 +28,37 @@ def rename_columns(X: pd.DataFrame):
     return X
 
 
-@hydra.main(version_base=None, config_path="../../config", config_name="main")
-def process_data(config: DictConfig):
+@task
+def split_train_test(X: pd.DataFrame, y: pd.DataFrame):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=7
+    )
+    return {
+        "X_train": X_train,
+        "X_test": X_test,
+        "y_train": y_train,
+        "y_test": y_test,
+    }
+
+
+@task
+def save_data(data: dict, config):
+    for name, df in data.items():
+        df.to_csv(config.processed[name], index=False)
+
+
+@flow
+def process_data():
     """Function to process the data"""
 
-    data = get_data(abspath(config.raw.path))
+    config = load_config()
+    data = get_data(config.raw.path)
 
     y, X = get_features(config.process.target, config.process.features, data)
 
     X = rename_columns(X)
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=7
-    )
-
-    # Save data
-    X_train.to_csv(abspath(config.processed.X_train.path), index=False)
-    X_test.to_csv(abspath(config.processed.X_test.path), index=False)
-    y_train.to_csv(abspath(config.processed.y_train.path), index=False)
-    y_test.to_csv(abspath(config.processed.y_test.path), index=False)
+    data = split_train_test(X, y)
+    save_data(data, config)
 
 
 if __name__ == "__main__":
